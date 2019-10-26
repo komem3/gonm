be50213a7aea9a208e03c0def6a09b89832bd425
komem3 <komata392@gmail.com>
2019-10-26 19:08:53 +0900

    fix: add datastore.client to FromContext argument

--- env.go
+++ env.go
@@ -14,13 +14,6 @@ func init() {
 	projectID = os.Getenv("DATASTORE_PROJECT_ID")
 }
 
-// SetProjectID set datastore project id.
-//
-// It is not necessary to use if the project ID is set in the environment variable DATASTORE_PROJECT_ID.
-func SetProjectID(ID string) {
-	projectID = ID
-}
-
 func getProjectID() string {
 	return projectID
 }
